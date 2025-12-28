6428fba26adfab3b53657946a4c2384092993ec6
Mohammed Ihsaan <mohammedihsaan28@gmail.com>
2025-12-28 18:23:57 +0530

    Implement Auth

---
+++ src/services/auth.service.js
@@ -0,0 +1,86 @@
+import logger from '#config/logger.js';
+import bcrypt from 'bcrypt';
+import { db } from '#config/database.js';
+import { users } from '#models/user.model.js';
+import { eq } from 'drizzle-orm';
+
+export const hashPassword = async password => {
+  try {
+    return await bcrypt.hash(password, 10);
+  } catch (e) {
+    logger.error('Error hashing password:', e);
+    throw new Error('Password hashing failed');
+  }
+};
+
+export const comparePassword = async (password, hashPassword) => {
+  try{
+    return await bcrypt.compare(password,hashPassword);
+  } catch(e){
+    logger.error('Error comparing the password',e);
+    throw new Error('Error comparing password');
+  }
+};
+
+export const authenticateUser = async (email, password) => {
+  try {
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const isPasswordValid = await comparePassword(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new Error('Invalid password');
+    }
+
+    logger.info(`User authenticated: ${email}`);
+    return user;
+  } catch (e) {
+    logger.error('Error authenticating user:', e);
+    throw e;
+  }
+};
+
+export const createUser = async ({ name, email, password, role = 'user' }) => {
+  try {
+    const existingUser = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+
+    if (existingUser.length > 0) {
+      throw new Error('User with this email already exists');
+    }
+    const passwordHash = await hashPassword(password);
+
+    const [newUser] = await db
+      .insert(users)
+      .values({
+        name,
+        email,
+        password: passwordHash,
+        role,
+      })
+      .returning({
+        id: users.id,
+        name: users.name,
+        email: users.email,
+        role: users.role,
+        created_at: users.createdAt,
+      });
+
+    logger.info(`Created new user: ${email}`);
+    return newUser;
+  } catch (e) {
+    logger.error('Error creating user:', e);
+    throw e;
+  }
+};
